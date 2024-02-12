1a0a97823db09fe3858be316d72c9a36fd25a814
zenoxs <amaury.civier@gmail.com>
2024-02-12 19:23:21 +0100

    improve render font code

--- src/graphics/sprite_text/render.rs
+++ src/graphics/sprite_text/render.rs
@@ -3,7 +3,7 @@ use image::{DynamicImage, ImageBuffer, Rgba, RgbaImage};
 
 use crate::graphics::{assets::font_assets::FontSheet, sprite_text::utils::extract_sub_image};
 
-use super::SpriteText;
+use super::{utils::calculate_glyph_positions, SpriteText};
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
 pub enum SpriteTextRenderSet {
@@ -48,63 +48,16 @@ pub(crate) fn render_texture(
 
             let texture_image = images.get(texture_atlas.texture.id()).unwrap();
 
-            let mut line_number = 1;
-            let mut max_width: u32 = 0;
-            let mut max_height: u32 = 0;
-            let mut dx: u32 = 0;
-            let mut dy: u32 = 0;
-            let space_width = 5;
-            let mut current_line_width = 0;
+            let (glyphs, max_width, max_height) = calculate_glyph_positions(
+                section,
+                font_sheet,
+                texture_atlas,
+                texture_image,
+                bounds,
+            );
 
-            let mut glyphs = Vec::new();
-            for character in section.value.chars() {
-                if character == '\n' {
-                    // Handle explicit line break
-                    dx = 0;
-                    dy += max_height;
-                    max_width = max_width.max(current_line_width);
-                    current_line_width = 0;
-                    max_height = 0; // Reset max height for the next line
-                    line_number += 1;
-                    continue;
-                }
+            let mut combined = RgbaImage::new(max_width, max_height);
 
-                let glyph_id = character as u32;
-                let (glyph_width, glyph_image) = if character == ' ' {
-                    (space_width, ImageBuffer::new(space_width, max_height)) // Use max_height for consistent line height
-                } else if let Some(glyph) = font_sheet.glyphs.get(&glyph_id) {
-                    let glyph_rect = texture_atlas.textures[glyph.index];
-                    let glyph_image = extract_sub_image(texture_image, &glyph_rect)
-                        .expect("Failed to extract sub-image");
-                    (glyph_image.width(), glyph_image)
-                } else {
-                    warn!("couldn't find the character '{}'", character);
-                    (space_width, ImageBuffer::new(space_width, 0))
-                };
-
-                max_height = max_height.max(glyph_image.height());
-
-                // Check if the current glyph exceeds the bounds and a line break is needed
-                if bounds.size.x < (dx + glyph_width) as f32 {
-                    dx = 0; // Reset dx for the new line
-                    dy += max_height; // Move dy to the next line
-                    line_number += 1;
-                    max_width = max_width.max(current_line_width);
-                    current_line_width = 0; // Reset current line width
-                    max_height = glyph_image.height(); // Reset max height for the new line
-                }
-
-                glyphs.push((glyph_id, glyph_image, dx, dy));
-
-                // Update dx and current line width
-                dx += glyph_width;
-                current_line_width = current_line_width.max(dx);
-            }
-
-            // After iterating through all characters, update max_width for the last line if needed
-            max_width = max_width.max(current_line_width);
-
-            let mut combined = RgbaImage::new(max_width, max_height * line_number);
             // Backgrounds
             let red = Rgba([255, 0, 0, 255]);
             for pixel in combined.pixels_mut() {
